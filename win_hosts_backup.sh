0689288a872a971962a5521cbf7284c4191f8cfc
Sacha Panasuik <sachapan@gmail.com>
2021-01-11 18:08:42 -0500

    Separated exclude lines to be more readable.

--- win_hosts_backup.sh
+++ win_hosts_backup.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-ssh sacha@slave2 "tar cvf - --exclude=AppData --exclude=OneDrive --exclude='Google Drive' --exclude='.vagrant.d' --exclude=.dotfiles.git --exclude=.oh-my-zsh --exclude=tmp --exclude=Videos --exclude='_.*' --exclude='NTUSER.DAT'  ." 2>/mnt/Archive TV/backups/slave2/backup_slave2_test.log | dd of=/mnt/Archive\ TV/backups/slave2/user_sacha_test.tar
+ssh sacha@slave2 "tar cvf - \
+    --exclude=AppData \
+    --exclude=OneDrive \
+    --exclude='Google Drive' \
+    --exclude='.vagrant.d' \
+    --exclude=.dotfiles.git \
+    --exclude=.oh-my-zsh \
+    --exclude=tmp \
+    --exclude=Videos \
+    --exclude='_.*' \
+    --exclude='NTUSER.DAT' \
+    ." \
+    2>/mnt/Archive\ TV/backup/slave2/backup_slave2_test.log \
+    | dd of=/mnt/Archive\ TV/backup/slave2/user_sacha_test.tar
 
