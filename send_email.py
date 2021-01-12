607b1032d777666d002e48d1fd88bb00b9f61ceb
Sacha Panasuik <sachapan@gmail.com>
2021-01-11 22:07:38 -0500

    Little one to send email in python.  From the old garage door monitor project that ran on a raspberry.

---
+++ send_email.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import smtplib
+
+SERVER = "localhost"
+
+FROM = "garage@huey.homeip.net"
+TO = ["sachapan@gmail.com"] # must be a list
+
+SUBJECT = "Hello!"
+
+TEXT = "This message was sent with Python's smtplib."
+
+# Prepare actual message
+
+message = """\
+From: %s
+To: %s
+Subject: %s
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+# Send the mail
+
+server = smtplib.SMTP(SERVER)
+server.sendmail(FROM, TO, message)
+server.quit()
