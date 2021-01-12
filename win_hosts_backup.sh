c4175599c6e3cfaf823c44bd1048ebeef43c1a98
Sacha Panasuik <sachapan@gmail.com>
2021-01-11 19:37:29 -0500

    Remove initial test host.

--- win_hosts_backup.sh
+++ win_hosts_backup.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
-ssh sacha@slave2 "tar cvf - \
-    --exclude=AppData \
-    --exclude=OneDrive \
-    --exclude='Google\ Drive' \
-    --exclude='.vagrant.d' \
-    --exclude=.dotfiles.git \
-    --exclude=.oh-my-zsh \
-    --exclude=tmp \
-    --exclude=Videos \
-    --exclude='_.*' \
-    --exclude='NTUSER.DAT' \
-    ." \
-    2>/mnt/Archive\ TV/backup/slave2/backup_slave2_test.log \
-    | dd of=/mnt/Archive\ TV/backup/slave2/slave2_`date +%a`.tar
+#ssh sacha@slave2 "tar cvf - \
+#    --exclude=AppData \
+#    --exclude=OneDrive \
+#    --exclude='Google\ Drive' \
+#    --exclude='.vagrant.d' \
+#    --exclude=.dotfiles.git \
+#    --exclude=.oh-my-zsh \
+#    --exclude=tmp \
+#    --exclude=Videos \
+#    --exclude='_.*' \
+#    --exclude='NTUSER.DAT' \
+#    ." \
+#    2>/mnt/Archive\ TV/backup/slave2/backup_slave2_test.log \
+#    | dd of=/mnt/Archive\ TV/backup/slave2/slave2_`date +%a`.tar
 
 ssh cryst@crystal-desktop "tar cvf - \
     --exclude=*/NTUSER* \
