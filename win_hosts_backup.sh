9a4afe827950cea2959ce4cf2f869d025d97bf41
Sacha Panasuik <sachapan@gmail.com>
2021-01-13 14:53:48 -0500

    Changed excludes to be read from a separate file.  Parameter: -t or --test directs destination to /dev/null.

--- win_hosts_backup.sh
+++ win_hosts_backup.sh
@@ -20,15 +20,15 @@ do
         -t|--test)
         TEST=1
         shift
+# Add -m parameter to force monthly backup run
     esac
 done
 date
-if $TEST 
-then
-    echo "This is just a test."
-    exit 1
-fi
-exit 1
+#if $TEST 
+#then
+#    echo "This is just a test."
+#    exit 1
+#fi
 echo "Beginning backup run for $HOST."
 if [ $DOM = $SPECIAL ]
 then
@@ -38,25 +38,33 @@ else
     echo "Today is a regular backup day."
     FILE=backup_$HOST-$DOW
 fi
-echo "backup will be stored at $DIR/$FILE.tar"
-echo "backup log will be stored at $DIR/$FILE.log"
-echo "Attempting to connect to $HOST as $USER."
-time ssh $USER@$HOST "tar cvf - \
-    --exclude=*/NTUSER* \
-    --exclude=*/AppData \
-    --exclude=*/Downloads \
-    --exclude=*/Google\ Drive \
-    --exclude=*/MicrosoftEdgeBackups \
-    --exclude=*/OneDrive/backups \
-    --exclude=*/Music \
-	--exclude=*/Videos \
-    /cygdrive/c/Users/" \
-    2>$DIR/$FILE.log \
-    | dd of=$DIR/$FILE.tar
-date
-ls -l $DIR/$FILE.*
-echo "The backup for today is concluded."
-echo "Thank you for your cooperation."
+BACKUP_FILE=$DIR/$FILE.tar
+BACKUP_LOG=$DIR/$FILE.log
+if $TEST
+then 
+    BACKUP_FILE=/dev/null
+    BACKUP_LOG=$DIR/test_$FILE.log
+fi
+    echo "Backup will be stored at $BACKUP_FILE"
+    echo "Backup log will be stored at $BACKUP_LOG"
+    echo "Attempting to connect to $HOST as $USER."
+    ssh $USER@$HOST "tar cvf - \
+     --exclude=*/NTUSER* \
+     --exclude=*/AppData \
+     --exclude=*/Downloads \
+     --exclude=*/Google\ Drive \
+     --exclude=*/MicrosoftEdgeBackups \
+     --exclude=*/OneDrive/backups \
+     --exclude=*/Music \
+     --exclude=*/Videos \
+     /cygdrive/c/Users/" \
+     2>$BACKUP_LOG \
+     | dd of=$BACKUP_FILE
+    date
+#    ls -l $DIR/$FILE.*
+    echo
+    echo "The backup for today is concluded."
+    echo "Thank you for your cooperation."
 exit 0
 #    2>/mnt/Archive\ TV/backup/crystal-desktop/backup_crystal-desktop.log \
 #    | dd of=/mnt/Archive\ TV/backup/crystal-desktop/crystal-desktop_`date +%a`.tar
