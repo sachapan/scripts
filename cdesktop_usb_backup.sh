df7824336fb699c1a71e168a9109747dc90f2517
Sacha Panasuik <sachapan@gmail.com>
2021-01-13 14:54:32 -0500

    cdesktop_usb_backup.sh now reads excludes from local exclude file.

--- cdesktop_usb_backup.sh
+++ cdesktop_usb_backup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 ssh cryst@crystal-desktop "tar cvf /cygdrive/d/custom_backups/users_backup_`date +%F`.tar \
-	--exclude="*/AppData" \
-	--exclude="*/Downloads" \
-	--exclude="Google Drive" \
-	/cygdrive/c/Users"
+	-X /home/cryst/crystal-desktop_excludes /cygdrive/c/Users"
+#	--exclude="*/AppData" \
+#	--exclude="*/Downloads" \
+#	--exclude="Google Drive" \
