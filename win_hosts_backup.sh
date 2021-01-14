d630f29d9ad20e41fd8549e8fa9052bdd2f67614
Sacha Panasuik <sachapan@gmail.com>
2021-01-14 09:52:45 -0500

    Realized default shell is dash instead of bash.  Refactoring newly added debug function.

--- win_hosts_backup.sh
+++ win_hosts_backup.sh
@@ -7,6 +7,7 @@ USER=cryst
 DIR="/mnt/backup/crystal-desktop"
 TARGET="/cygdrive/c/Users/"
 EXCLUDE="/home/cryst/crystal-desktop_excludes"
+NOSSH=0
 # Day of the month to create the monthly backup
 SPECIAL=05
 DOM=`date +%d`
@@ -17,6 +18,25 @@ if [ $DOM = $SPECIAL ]
 then
     MONTHLY=1
 fi
+function var_debug () {
+    echo "var_debug was called."
+    echo "variables:"
+    echo "\t TEST = $TEST"
+    echo "\t NOSSH = $NOSSH"
+    echo "\t HOST = $HOST"
+    echo "\t USER = $USER"
+    echo "\t DIR = $DIR"
+    echo "\t TARGET = $TARGET"
+    echo "\t EXCLUDE = $EXCLUDE"
+    echo "\t BACKUP_FILE = $BACKUP_FILE"
+    echo "\t BACKUP_LOG = $BACKUP_LOG"
+    echo "\t DOM = $DOM"
+    echo "\t DOW = $DOW"
+    echo "\t MONTHLY = $MONTHLY"
+    echo "\t FULLDATE = $FULLDATE"
+    echo "\t SPECIAL = $SPECIAL"
+exit 1
+}
 # Check for parameters
 #whole getopts t: flag
 echo "received command line parameters: $@"
@@ -31,6 +51,10 @@ do
         -m|--monthly)
         MONTHLY=1
         shift;;
+        -n) OUTPUT=${OPTARG};;
+        -n|--nossh)
+        NOSSH=1
+        shift;;
 # Add -m parameter to force monthly backup run
     esac
 done
@@ -41,7 +65,7 @@ date
 #    exit 1
 #fi
 echo "Beginning backup run for $HOST."
-if $MONTHLY
+if [ $MONTHLY = 1 ]
 then
     echo "Today is a special backup day: performing monthly."
     FILE=backup_$HOST-$FULLDATE
@@ -51,7 +75,7 @@ else
 fi
 BACKUP_FILE=$DIR/$FILE.tar
 BACKUP_LOG=$DIR/$FILE.log
-if $TEST
+if [ $TEST = 1 ];
 then 
     BACKUP_FILE=/dev/null
     BACKUP_LOG=$DIR/test_$FILE.log
@@ -59,12 +83,17 @@ fi
     echo "Backup will be stored at $BACKUP_FILE"
     echo "Backup log will be stored at $BACKUP_LOG"
     echo "Attempting to connect to $HOST as $USER."
-    ssh $USER@$HOST "tar cvf - \
-    -X $EXCLUDE \
-     $TARGET " \
-     2>$BACKUP_LOG \
-     | dd of=$BACKUP_FILE
+if [ $NOSSH = 0 ]
+then
+    echo "we fell into the NOSSH=0 condition."
+    echo "MONTHLY=$MONTHLY"
+    var_debug
+#    ssh $USER@$HOST "tar cvf - -X $EXCLUDE $TARGET" 2>$BACKUP_LOG | dd of=$BACKUP_FILE
     date
+else
+    echo "Ok, with the nossh option, this is where we end."
+    var_debug
+fi
 #    ls -l $DIR/$FILE.*
     echo
     echo "The backup for today is concluded."
