5f5a5dfb7380314c90363a1ec1bbc1323f6033fc
Sacha Panasuik <sachapan@gmail.com>
2021-01-14 16:11:29 -0500

    This is basically a oneliner.

--- cdesktop_usb_backup.sh
+++ cdesktop_usb_backup.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 ssh cryst@crystal-desktop "tar cvf /cygdrive/d/custom_backups/users_backup_`date +%F`.tar \
 	-X /home/cryst/crystal-desktop_excludes /cygdrive/c/Users"
-#	--exclude="*/AppData" \
-#	--exclude="*/Downloads" \
-#	--exclude="Google Drive" \
