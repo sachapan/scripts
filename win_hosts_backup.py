a9661bfdc55b882f226ae102f0e3dc444d493748
Sacha Panasuik <sachapan@gmail.com>
2021-01-15 14:14:12 -0500

    Initial outline for python version of win_hosts_backup script.

---
+++ win_hosts_backup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# A python version of the /bin/sh script win_hosts_backup.sh
+# Purpose: perform backups via ssh, tar and dd from a remote host to the local host
+#
+# Sacha Panasuik
+# sachapan@gmail.com
+# Initial script creation: January 14, 2021
+
+# Set some variables
+# Read variables from config file?
+
+# Read command line flags/parameters
+
+# determine if a monthly backup should be performed
+
+# perform backup
+
+# report backup results
+
+
+if __name__ == "__main__":
+    main()
