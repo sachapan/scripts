12d92dbcdc916354329a3043ff2927e8fda46c48
Sacha Panasuik <sachapan@gmail.com>
2021-01-19 09:58:03 -0500

    Changed output file to day of week instead of full date to allow for outwriting without having to rotate tgz files.

--- cdesktop_usb_backup.sh
+++ cdesktop_usb_backup.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-ssh cryst@crystal-desktop "tar cvzf /cygdrive/d/custom_backups/users_backup_`date +%F`.tgz \
+#ssh cryst@crystal-desktop "tar cvzf /cygdrive/d/custom_backups/users_backup_`date +%F`.tgz \
+ssh cryst@crystal-desktop "tar cvzf /cygdrive/d/custom_backups/users_backup_`date +%a`.tgz \
 	-X /home/cryst/crystal-desktop_excludes /cygdrive/c/Users"
