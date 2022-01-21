7ad6e51cd99d99f64e1ea2d99fd6c8339ec1e318
Sacha Panasuik <sachapan@gmail.com>
2022-01-21 11:51:06 -0500

    Added quiet flag to win_hosts_backup.sh

--- win_hosts_backup.sh
+++ win_hosts_backup.sh
@@ -28,6 +28,7 @@ FULLDATE=`date +%F`
 MONTHLY=0
 MONTHLY_FLAG=0
 NOSSH=0
+QUIET=0
 SECONDS=0
 TEST=0
 VERBOSE=0
@@ -41,6 +42,12 @@ func_separator(){
   # this function simply prints a pretty (or ugly) line
   echo "-------------------------------------------------------------"
 }    
+func_quiet_echo(){
+if [ $QUIET = 0 ]
+then
+  echo $1
+fi
+}
 func_var_dump(){
 # this function is for debugging purposes.  It dumps internal variables and then exits the script.
   func_separator
@@ -53,6 +60,7 @@ func_var_dump(){
   echo " FULLDATE = $FULLDATE"
   echo " SPECIAL = $SPECIAL"
   echo " DIR = $DIR"
+  echo " QUIET = $QUIET"
   echo " TARGET = $TARGET"
   echo " EXCLUDE = $EXCLUDE"
   echo " BACKUP_FILE = $BACKUP_FILE"
@@ -60,6 +68,7 @@ func_var_dump(){
   echo " MONTHLY = $MONTHLY"
   echo " TEST = $TEST"
   echo " NOSSH = $NOSSH"
+  echo " QUIET = $QUIET"
   echo " VERBOSE = $VERBOSE"
   exit 1
 }
@@ -72,40 +81,44 @@ do
       TEST=1
       shift
     ;;
+    -q|--quiet)
+      QUIET=1
+      shift
+    ;;
     -b|--backup)
       TARGET=$2
-      echo "Backup target is: $TARGET"
+      func_quiet_echo "Backup target is: $TARGET"
       shift
       shift
     ;;
     -u|--user)
       USER=$2
-      echo "Remote user is: $USER"
+      func_quiet_echo "Remote user is: $USER"
       shift
       shift
     ;;
     -x|--exclude)
       EXCLUDE=$2
-      echo "EXCLUDE file is: $EXCLUDE"
+      func_quiet_echo "EXCLUDE file is: $EXCLUDE"
       shift
       shift
     ;;
-    -r|--remote)
+   -r|--remote)
       HOST=$2
-      echo "Remote host is: $HOST"
+      func_quiet_echo "Remote host is: $HOST"
       shift
       shift
     ;;
     -d|--dir)
       DIR=$2
-      echo "Local directory target is: $DIR"
+      func_quiet_echo "Local directory target is: $DIR"
       shift
       shift
     ;;
     -m|--monthly)
       MONTHLY=1
       MONTHLY_FLAG=1
-      echo "Monthly flag set from command line."
+      func_quiet_echo "Monthly flag set from command line.  Otherwise the monthly backup is run on day $SPECIAL"
       shift
       ;;
     -n|--nossh)
@@ -125,6 +138,7 @@ do
       echo "-h or --help or --usage       Print this help text."
       echo "-m or --monthly               Force a monthly backup run."
       echo "-n or --nossh                 Do not connect with ssh - i.e. perform local backup."
+      echo "-q or --quiet                 Minimize output text."
       echo "-r host or --remote host      The remote host to connect to."
       echo "                              Default is $HOST."
       echo "-t or --test                  Output log file prepended with 'test_' and direct "
@@ -150,8 +164,11 @@ do
   esac
 done
 echo "Beginning backup run for $HOST."
-date
-func_separator
+if [ $QUIET = 0 ]
+then
+  date
+  func_separator
+fi
 if [ $MONTHLY = 1 ]
 then
   if [ $MONTHLY_FLAG = 1 ]
@@ -161,7 +178,7 @@ then
   echo "Today is a special backup day: performing monthly."
   FILE=backup_$HOST-$FULLDATE
 else
-  echo "Today is a regular backup day: performing daily."
+  func_quiet_echo "Today is a regular backup day: performing daily."
   FILE=backup_$HOST-$DOW
 fi
 BACKUP_FILE=$DIR/$FILE.tar
@@ -176,27 +193,27 @@ if [ ! -d $DIR ];
 then 
     mkdir $DIR
 fi
-echo "Backup will be stored at $BACKUP_FILE"
-echo "Backup log will be stored at $BACKUP_LOG"
+func_quiet_echo "Backup will be stored at $BACKUP_FILE"
+func_quiet_echo "Backup log will be stored at $BACKUP_LOG"
 if [ $NOSSH = 0 ]
   then
 #    func_var_dump
 # TODO: Add test for successful ssh connection to limit null backup overwriting a good one.
-  echo "Attempting to connect to $HOST as $USER."
-  func_separator
+  func_quiet_echo "Attempting to connect to $HOST as $USER."
+#  func_separator
   ssh -q $USER@$HOST "exit"
   if [ $? == 0 ]
     then
-       echo "Connection successful."
+       func_quiet_echo "Connection successful."
        echo "Performing Backup."
-       ssh -q $USER@$HOST "tar cvf - -X $EXCLUDE $TARGET" 2>$BACKUP_LOG | dd of=$BACKUP_FILE
+       ssh -q $USER@$HOST "tar cvf - -X $EXCLUDE $TARGET" 2>$BACKUP_LOG | dd status="none" of=$BACKUP_FILE
     else
        echo "Connection failed."
        exit 1
     fi
   else
-  echo "Ok, the nossh option means local backup has been enabled."
-  tar cvf - -X $EXCLUDE $TARGET 2>$BACKUP_LOG | dd of=$BACKUP_FILE
+  func_quiet_echo "Ok, the nossh option means local backup has been enabled."
+  tar cvf - -X $EXCLUDE $TARGET 2>$BACKUP_LOG | dd status="none" of=$BACKUP_FILE
 fi
 SIZE=`du -hs $BACKUP_FILE | awk '{print $1}'`
 echo "Backup File size: $SIZE"
@@ -204,9 +221,9 @@ if [ $VERBOSE = 1 ]
 then
   cat $BACKUP_LOG
 fi
-echo "The backup for today is concluded."
-date
+func_quiet_echo "The backup for today is concluded."
+#date
 duration=$SECONDS
 echo "$(($duration / 60)) minutes and $(($duration % 60)) seconds to complete."
-echo "Thank you for your cooperation."
+func_quiet_echo "Thank you for your cooperation."
 exit 0
