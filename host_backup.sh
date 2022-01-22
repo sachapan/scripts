e9ca7b7671850587d631619b5ce83582db644c7a
Sacha Panasuik <sachapan@gmail.com>
2022-01-22 08:42:08 -0500

    Added quiet flag.

--- host_backup.sh
+++ host_backup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Little one to pipe commands into my usual backup script.  Good for crontab use.
-~sacha/bin/win_hosts_backup.sh -b '/opt /etc /home' -n -d /mnt/backup/`hostname` \
+~sacha/bin/win_hosts_backup.sh -q -b '/opt /etc /home' -n -d /mnt/backup/`hostname` \
 -x /mnt/backup/`hostname`/backup.excludes | /usr/local/bin/telegram-send --stdin -g
 
