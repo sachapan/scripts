322d3070fc5741c225fa20f7c86446c03e5b8a99
Sacha Panasuik <sachapan@gmail.com>
2022-05-11 09:43:50 -0400

    New vlc script.

---
+++ play_artists_roll.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env zsh
+# MacOS script
+# Auto repeat a vlc video - intended to be added to login items to autostart
+/Applications/VLC.app/Contents/MacOS/VLC --no-osd -R ~/Documents/artist\ slideshow\ 5.8\ 720.mp4
