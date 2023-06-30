c68eb5b2351ffab67980fc84e1ffd23670244c04
Sacha Panasuik <sachapan@gmail.com>
2023-06-30 16:55:24 -0400

    Added qrdecode.py script.

---
+++ qrdecode.py
@@ -0,0 +1,16 @@
+import os
+from pyzbar.pyzbar import decode
+from PIL import Image
+
+def main():
+    while True:
+        input_file = input("QR code filename to decode: ")
+        if os.path.isfile(input_file):
+            break
+        else:
+            print("No such file!")
+    decodecQR = decode(Image.open(input_file))
+    print(decodecQR[0].data.decode('ascii'))
+
+if __name__ == "__main__":
+    main()
