ccbbbc9c53bf664aade2bbbfbf8f61a23876c11d
Sacha Panasuik <sachapan@gmail.com>
2024-01-22 14:47:06 -0500

    Accept command line file name or else prompt for one in qrdecode.py script.

--- qrdecode.py
+++ qrdecode.py
@@ -1,15 +1,20 @@
 #!/usr/bin/env python3
 import os
+import sys
 from pyzbar.pyzbar import decode
 from PIL import Image
 
 def main():
     while True:
-        input_file = input("QR code filename to decode: ")
+        if (args_count := len(sys.argv)) < 2:
+            input_file = input("QR code filename to decode: ")
+        else:
+            input_file = sys.argv[1]
         if os.path.isfile(input_file):
             break
         else:
             print("No such file!")
+            exit(1)
     decodecQR = decode(Image.open(input_file))
     print(decodecQR[0].data.decode('ascii'))
 
